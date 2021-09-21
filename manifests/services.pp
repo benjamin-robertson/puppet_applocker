2fc29e45614cc6e12e87405e0bcc1a19c391f029
Benjamin Robertson <ben.robertson@puppet.com>
2021-09-21 13:40:04 +1000

    Removed AppID

--- manifests/services.pp
+++ manifests/services.pp
@@ -7,17 +7,12 @@
 class puppet_applocker::services {
 
   # Start the services
-  service { 'AppID':
-    ensure   => 'running',
-    enable   => 'true',
-    provider => 'windows',
-  }
-  #service { 'AppIDSvc':
-  #  ensure       => 'running',
-  #  enable       => 'true',
-  #  logonaccount => 'NT Authority\LocalService',
-  #  provider     => 'windows',
+  #service { 'AppID':
+  #  ensure   => 'running',
+  #  enable   => 'true',
+  #  provider => 'windows',
   #}
+
   registry::value { 'HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\AppIDSvc\Start':
     key    => 'HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\AppIDSvc',
     value  => 'Start',
